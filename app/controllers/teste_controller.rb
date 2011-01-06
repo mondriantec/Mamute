fca1d1f543f39b2df69a8150740e7196a33c063d
Coutinho <coutinho@mondriantecnologia.com>
2011-01-05 21:15:36 -0300

    removendo o freeze do rails

---
+++ app/controllers/teste_controller.rb
@@ -0,0 +1,5 @@
+class TesteController < ApplicationController
+  def index
+  end
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
 
   config.gem "sqlite3-ruby", :lib => "sqlite3"
-  config.gem "brazilian-rails"
+  config.gem "brazilian-rails", :version => '2.1.15'
   config.time_zone = 'UTC'
 
 end
