142a3f5bed01343fd8734b33d9ebec9faca61b00
Coutinho <coutinho@mondriantecnologia.com>
2011-02-23 11:22:35 -0300

    home

--- app/models/cartorio.rb
+++ app/models/cartorio.rb
@@ -1,4 +1,7 @@
 class Cartorio < ActiveRecord::Base
   belongs_to :cidade
-  has_many :usuarios, :as => :entidade
+  has_many :usuarios, :as => :entidade     
+  def nome
+     self.oficio
+  end
 end
