1960ad562b6b4e2eda32aa464a32cfec3f4c1cf9
Robson <robson@robson-HP-Pavilion-dv5000-EZ406UA-ABA.(none)>
2011-06-14 10:39:26 -0300

    cartorio

--- app/models/notificacao.rb
+++ app/models/notificacao.rb
@@ -2,4 +2,12 @@ class Notificacao < ActiveRecord::Base
   belongs_to :cidade
   belongs_to :motivo_notificacao	
   belongs_to :notificado, :class_name => 'Usuario', :foreign_key => 'notificado_id'
+  belongs_to :cartorio
+
+  before_save :setar_cartorio
+
+  private
+  def setar_cartorio
+    self.cartorio = Cartorio.first
+  end
 end
