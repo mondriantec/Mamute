3b0bad553a9c294f873befd7ea6c768e8d4da189
Luiz Magno <magno@mondriantecnologia.com>
2011-10-20 09:54:39 -0300

    ajustando o edit dos documentos

--- app/models/campo_documento.rb
+++ app/models/campo_documento.rb
@@ -1,4 +1,9 @@
 class CampoDocumento < ActiveRecord::Base
   belongs_to :tipo_documento
   has_many :campo_documentos
+
+  def valor_campo(documento)
+   x = ValorCampoDocumento.find_by_documento_id_and_campo_documento_id documento.id, self.id
+   x ? x.valor : ''
+  end  
 end
