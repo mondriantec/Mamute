da2cfd291296c0b048bab7fe9e14e2a78b3c753b
Luiz Magno <magno@mondriantecnologia.com>
2011-11-17 13:34:56 -0300

    correcao da trigger

--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -4,6 +4,14 @@ class Fatura < ActiveRecord::Base
   has_one :boleto
 
 
+  def descricao_status
+    case self.status 
+     when 0: 'Gerada'
+     when 1: 'Boleto Pago Menor'
+     when 2: 'Boleto Pago Ok'
+    end
+  end
+
   def self.descartar_fatura_pendente(uf)
      #apagar os boletos, as faturas e desmarcar os documentos 
   end
